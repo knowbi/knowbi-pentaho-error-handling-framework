e563e940554bf89d7f03b748699b3b8b94d82a4a
Lokke <lokke.bruyndonckx@know.bi>
2017-04-25 19:58:37 +0200

    Added Error mail and Error table

---
+++ sql/tb_log.sql
@@ -0,0 +1,10 @@
+/*
+* Table Name: tb_log
+* Version History:
+* 1.0 25/04/2017 LB Initial version
+*/
+CREATE TABLE tb_log (
+	error_dttm timestamp NULL,
+	error_desc varchar(300) NULL,
+	transformation_name varchar(300) NULL
+);
